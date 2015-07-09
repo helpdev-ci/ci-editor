b818365fab05c920b06388906c0d1e25bd5a6de8
Komsan Nurak <n.komsan@gmail.com>
2015-07-10 06:41:36 +0700

    Change application path

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = 'ci/system';
+	$system_path = '../system';
 
 /*
  *---------------------------------------------------------------
@@ -72,7 +72,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'ci/application';
+	$application_folder = 'ci-editor';
 
 /*
  * --------------------------------------------------------------------
